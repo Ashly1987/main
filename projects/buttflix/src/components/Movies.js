ce07be166cbb953ad872e52da987571fdc0e9544
Ash <ash@Ashs-MacBook-Air.local>
2026-03-09 23:20:44 +0530

    move buttflix into projects folder

--- projects/buttflix/src/components/Movies.js
+++ projects/buttflix/src/components/Movies.js
@@ -5,6 +5,8 @@ import MovieCard from './MovieCard';
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [genre, setGenres] = useState('All Genres');
+  const [rating, setRating] = useState('All');
 
   useEffect(() => {
     //const loadMovies = ["avengers", "inception", "interstellar", "the dark knight", "the matrix", "pulp fiction", "fight club", "forrest gump", "the lord of the rings", "the godfather"];
@@ -18,20 +20,81 @@ export default function MoviesGrid() {
     setSearchTerm(e.target.value);
   };
 
-  const filteredMovies = movies.filter((movie) =>
-    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  const handleGenreChange = (e) => {
+    setGenres(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
+  const mathchesGenre = (movie, genre) => {
+    if (genre === 'All Genres') return true;
+    return movie.genre.toLowerCase() === genre.toLowerCase();
+  };
+
+  const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const matchesRating = (movie, rating) => {
+    switch (rating) {
+      case 'Good':
+        return movie.rating >= 8;
+      case 'OK':
+        return movie.rating >= 5 && movie.rating < 8;
+      case 'Bad':
+        return movie.rating < 5;
+      default:
+        return true;
+    }
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      mathchesGenre(movie, genre) &&
+      matchesRating(movie, rating) &&
+      matchesSearchTerm(movie, searchTerm)
   );
 
   return (
     <div>
-      <div>
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search movies..."
-          value={searchTerm}
-          onChange={handleSearch}
-        />
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre:</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option value="All Genres">All Genres</option>
+            <option value="Action">Action</option>
+            <option value="Drama">Drama</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Horror">Horror</option>
+          </select>
+        </div>
+        <div className="filter-slot">
+          <label>Rating:</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>OK</option>
+            <option>Bad</option>
+          </select>
+        </div>
       </div>
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
